a16ea0c224f651fd70f005917e0c5596dc73042d
WILLIAN <willianlealdasiveira@outlook.com.br>
2020-03-07 16:30:02 -0300

    primeiro commit

---
+++ projeto06/src/fai/projeto06/Main.java
@@ -0,0 +1,18 @@
+package fai.projeto06;
+
+public class Main {
+
+	
+		public static void main(String[] args) {
+			Main app= new Main();
+			app.Carro();
+
+	}
+		private void Carro() {
+			
+				  
+				  
+	  }
+
+
+}
